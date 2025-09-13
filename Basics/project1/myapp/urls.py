45dc060b233491b0ab7d63d069d0a78130929545
goutam-waghe <goutamwaghe@gmail.com>
2025-09-13 16:29:13 +0530

    complete basic setup and urls Mapping

--- Basics/project1/myapp/urls.py
+++ Basics/project1/myapp/urls.py
@@ -4,5 +4,8 @@ from myapp import views
 
 urlpatterns = [
     path('' , views.home , name="home"),
+    path('about/' , views.about , name="about"),
+    path('service/' , views.service , name="service"),
+    path('contact/' , views.contact , name="contact"),
     path("student/", views.student, name="student")
 ]
--- Basics/project1/myapp/views.py
+++ Basics/project1/myapp/views.py
@@ -3,7 +3,19 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("<h1>Hellow</h1>")
+    # return HttpResponse("<h1>Hellow</h1>")
+    return render(request ,'myapp/home.html')
+
+def about(request):
+    return render(request ,'myapp/about.html')
+
+def service(request):
+    return render(request ,'myapp/service.html')
+
+def contact(request):
+    return render(request ,'myapp/contact.html')
+
+
 def student(request):
     name = "goutam"
     return render(request , 'myapp/index.html' , {'name':name})
